505d6f5d5a530c45b0ac2b50677bcf504ed8b883
carlosg14 <169074726+carlosg14@users.noreply.github.com>
2024-05-26 20:11:11 -0300

    Ejercicio secuencial 1- Promedio de notas

---
+++ Ejercicio_secuencial1.py
@@ -0,0 +1,19 @@
+"""EJERCICIO 1: Un estudiante está cursando 5 materias, tiene la nota de cada
+
+materia y quiere saber cuál es su promedio."""
+
+
+numero1 = int (input ("ingrese la primer nota: "))
+numero2 = int (input ("ingrese la segunda nota: "))
+numero3 = int (input ("ingrese la tercera nota: "))
+numero4 = int (input ("ingrese la cuarta nota: "))
+numero5 = int (input ("ingrese la quinta nota: "))
+
+
+suma = numero1 + numero2 + numero3 + numero4 + numero5
+
+promedio= suma / 5
+
+print ("el promedio de sus 5 notas es:", promedio)
+
+
